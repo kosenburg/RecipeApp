73abca7f931e419693d350c6741aed5a964258cc
Kevin <kosenburg@gmail.com>
2018-04-26 12:42:04 -0400

    Refactorted Main boot class name

--- src/main/java/com/webapps/recipe/RecipeApplication.java
+++ src/main/java/com/webapps/recipe/RecipeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GurumvcApplication {
+public class RecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GurumvcApplication.class, args);
+		SpringApplication.run(RecipeApplication.class, args);
 	}
 }
